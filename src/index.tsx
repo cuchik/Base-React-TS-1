ab03c784255d05c05ab52645129c51feb5f07b9d
Luan Nguyen <luan.nguyen10692@gmail.com>
2022-10-26 17:16:49 +0700

    update css

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
-
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { persistor, store } from './store';
-import './index.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
